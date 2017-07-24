4df0fd944d5223044b34d4dfb6eda6644b63e424
Corwin Crownover <corwin.crownover@gmail.com>
2017-07-24 14:44:51 -0700

    Make radio checked val reflect state

--- src/containers/Form.js
+++ src/containers/Form.js
@@ -11,11 +11,20 @@ class Form extends React.Component {
 			reportInfo: {
 				1: '',
 				2: '',
-				3: ''
+				3: null,
+				4: '',
+				5: '',
+				6: '',
+				7: '',
+				8: '',
+				9: null,
+				10: '',
+				11: ''
 			}
 		};
 
 		this.handleInput = this.handleInput.bind(this);
+		this.handleRadioChange = this.handleRadioChange.bind(this);
 	}
 
 	handleInput(e) {
@@ -24,19 +33,33 @@ class Form extends React.Component {
 		const input = e.target.value;
 		const sectionState = this.state[sectionKey]; 
 		sectionState[num] = input;
-		this.setState({ [sectionKey]: sectionState }, () => {console.log('state:', this.state)});
+		this.setState({ [sectionKey]: sectionState }, () => {console.log('section state:', this.state[sectionKey])});
 	}
+
+	handleRadioChange(e) {
+		console.log('name, val:', e.target.name, e.target.value);
+		const sectionKey = e.target.dataset.section;
+		const sectionState = this.state[sectionKey];
+		sectionState[e.target.name] = e.target.value;
+		this.setState({ [sectionKey]: sectionState }, () => {console.log('section state:', this.state[sectionKey])});
+		// ALLOW TOGGLE ON/OFF
+	}
+
+	//handleSubmit
 
 	render() {
 		return (
 			<ReportInfoView
 					handleInput={this.handleInput}
+					handleRadioChange={this.handleRadioChange}
 					input={this.state.reportInfo}/>
 		);
 	}
 }
 
 export default Form;
-// NEXT: LOAD PIECE OF STATE INTO EACH SECTION // HELPER FUNCTION FOR CHUNKING
-// NEXT: create controlled component
+// NEXT: controlled component for radio selection
+// NEXT: Handle each section name
+// NEXT: Create each section.
+// NEXT: Handle checkbox selection
 // TODO: Alter formview to take children / consolidate.
