eea3363aced0a3a2f9dbe6fe8e0c0685d279d981
Corwin Crownover <corwin.crownover@gmail.com>
2017-08-05 13:00:54 +0800

    Apply TitleLabel

--- src/components/ComplainantInfoView.js
+++ src/components/ComplainantInfoView.js
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 import { Checkbox } from 'react-bootstrap';
 
 import SectionView from './SectionView';
-import { Row, TextInput, Label, InputWrapper } from '../shared/Layout';
+import { Row, TextInput, InputWrapper, TitleLabel } from '../shared/Layout';
 import { FLEX } from '../shared/Consts';
 
 const ComplainantInfoView = ({ section, handleInput, input }) => {
@@ -15,19 +15,19 @@ const ComplainantInfoView = ({ section, handleInput, input }) => {
     <SectionView header="Complainant/Next of Kin Information">
       <Row>
         <InputWrapper flex={FLEX['1_4']}>
-          <Label>27. Complainant Name (Last, First, MI)</Label>
+          <TitleLabel>27. Complainant Name (Last, First, MI)</TitleLabel>
           <TextInput data-section={section} name='27a' value={input['27a']} onChange={handleInput} />
         </InputWrapper>
         <InputWrapper flex={FLEX['1_4']}>
-          <Label>Address (Apt Number, City, County, State, Zip)</Label>
+          <TitleLabel>Address (Apt Number, City, County, State, Zip)</TitleLabel>
           <TextInput data-section={section} name='27b' value={input['27b']} onChange={handleInput} />
         </InputWrapper>
         <InputWrapper flex={FLEX['1_4']}>
-          <Label>Relationship to Consumer</Label>
+          <TitleLabel>Relationship to Consumer</TitleLabel>
           <TextInput data-section={section} name='27c' value={input['27c']} onChange={handleInput} />
         </InputWrapper>
         <InputWrapper flex={FLEX['1_4']}>
-          <Label>Phone Number</Label>
+          <TitleLabel>Phone Number</TitleLabel>
           <TextInput data-section={section} name='27d' value={input['27d']} onChange={handleInput} />
         </InputWrapper>
       </Row>
