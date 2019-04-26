301aff7e0ce1d6499d3b7db9e88d4718900a2b6a
Solomon Tang <solomoncztang@gmail.com>
2019-04-25 17:03:49 -0700

    Remove unused import

--- src/components/controls/StyledRadio.js
+++ src/components/controls/StyledRadio.js
@@ -3,7 +3,7 @@
  */
 
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 export const RadioInputContainer = styled.input.attrs({
   type: 'radio'
