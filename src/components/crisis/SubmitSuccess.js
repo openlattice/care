8f86b44a2082d2cc3d38c2caf782ec4016a6b94b
Solomon Tang <solomoncztang@gmail.com>
2019-04-30 16:05:00 -0700

    Add clearAndNavigate

--- src/components/crisis/SubmitSuccess.js
+++ src/components/crisis/SubmitSuccess.js
@@ -2,10 +2,16 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Button } from 'lattice-ui-kit';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import type { Dispatch } from 'redux';
 
 import { FormWrapper } from './FormComponents';
 import { BLACK } from '../../shared/Colors';
 import { CRISIS_PATH, HOME_PATH } from '../../core/router/Routes';
+import { clearCrisisTemplate } from '../../containers/crisis/CrisisActionFactory';
+import { goToPath } from '../../core/router/RoutingActions';
+import type { RoutingAction } from '../../core/router/RoutingActions';
 
 const SubmittedView = styled.div`
   width: 100%;
@@ -37,13 +43,20 @@ const StyledButton = styled(Button)`
 `;
 
 type Props = {
-
+  actions :{
+    clearCrisisTemplate :() => {
+      type :string;
+    };
+    goToPath :(path :string) => RoutingAction;
+  };
 };
 
 class SubmitSuccess extends Component<Props> {
 
   clearAndNavigate = (path :string) => () => {
-
+    const { actions } = this.props;
+    actions.clearCrisisTemplate();
+    actions.goToPath(path);
   };
 
   render() {
@@ -61,4 +74,13 @@ class SubmitSuccess extends Component<Props> {
   }
 }
 
-export default SubmitSuccess;
+const mapDispatchToProps = (dispatch :Dispatch<*>) => ({
+  // $FlowFixMe
+  actions: bindActionCreators({
+    clearCrisisTemplate,
+    goToPath,
+  }, dispatch)
+});
+
+// $FlowFixMe
+export default connect(null, mapDispatchToProps)(SubmitSuccess);
