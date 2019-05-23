7891931140932dcceda94c20657a521da9e41792
Solomon Tang <solomoncztang@gmail.com>
2019-05-23 16:49:13 -0700

    New SearchPeopleWorker

--- src/containers/people/PeopleSagas.js
+++ src/containers/people/PeopleSagas.js
@@ -2,8 +2,13 @@
  * @flow
  */
 
-import moment from 'moment';
-import { call, put, takeEvery } from '@redux-saga/core/effects';
+import { DateTime } from 'luxon';
+import {
+  call,
+  put,
+  select,
+  takeEvery
+} from '@redux-saga/core/effects';
 import { Map } from 'immutable';
 import {
   EntityDataModelApi,
@@ -20,38 +25,30 @@ import {
   searchPeople
 } from './PeopleActions';
 
+import * as FQN from '../../edm/DataModelFqns';
 import {
-  PERSON_DOB_FQN,
-  PERSON_LAST_NAME_FQN,
-  PERSON_FIRST_NAME_FQN
-} from '../../edm/DataModelFqns';
-
-import { APP_TYPES_FQNS } from '../../shared/Consts';
+  getPeopleESId,
+} from '../../utils/AppUtils';
+import { isDefined } from '../../utils/LangUtils';
+import { ERR_ACTION_VALUE_NOT_DEFINED, ERR_ACTION_VALUE_TYPE } from '../../utils/Errors';
 
 const { OPENLATTICE_ID_FQN } = Constants;
-const { PEOPLE_FQN } = APP_TYPES_FQNS;
-
-const getPersonEntitySetId = (app) => {
-  const selectedOrganizationId = app.get('selectedOrganizationId');
-
-  return app.getIn([
-    PEOPLE_FQN.toString(),
-    'entitySetsByOrganization',
-    selectedOrganizationId
-  ]);
-};
 
 function* searchPeopleWorker(action :SequenceAction) :Generator<*, *, *> {
-  const { id, value } = action;
-  const {
-    firstName,
-    lastName,
-    dob,
-    app
-  } = value;
 
   try {
-    yield put(searchPeople.request(id));
+    const { value } = action;
+    if (!isDefined(value)) throw ERR_ACTION_VALUE_NOT_DEFINED;
+    if (!Map.isMap(value)) throw ERR_ACTION_VALUE_TYPE;
+
+    yield put(searchPeople.request(action.id));
+
+    const edm :Map<*, *> = yield select(state => state.get('edm'));
+    const app = yield select(state => state.get('app', Map()));
+
+    const firstNamePTID :UUID = edm.getIn(['fqnToIdMap', FQN.PERSON_FIRST_NAME_FQN]);
+    const lastNamePTID :UUID = edm.getIn(['fqnToIdMap', FQN.PERSON_LAST_NAME_FQN]);
+    const dobPTID :UUID = edm.getIn(['fqnToIdMap', FQN.PERSON_DOB_FQN]);
 
     const searchFields = [];
     const updateSearchField = (searchString :string, property :string, exact? :boolean) => {
@@ -62,37 +59,40 @@ function* searchPeopleWorker(action :SequenceAction) :Generator<*, *, *> {
         exact: true
       });
     };
-    if (firstName.trim().length) {
-      const firstNameId = yield call(EntityDataModelApi.getPropertyTypeId, PERSON_FIRST_NAME_FQN);
-      updateSearchField(firstName.trim(), firstNameId);
+
+    const firstName :string = value.get('firstName', '').trim();
+    const lastName :string = value.get('lastName', '').trim();
+    const dob :string = value.get('dob', '').trim();
+
+    if (firstName.length) {
+      updateSearchField(firstName, firstNamePTID);
     }
-    if (lastName.trim().length) {
-      const lastNameId = yield call(EntityDataModelApi.getPropertyTypeId, PERSON_LAST_NAME_FQN);
-      updateSearchField(lastName.trim(), lastNameId);
+    if (lastName.length) {
+      updateSearchField(lastName, lastNamePTID);
     }
-    if (dob && dob.trim().length) {
-      const dobMoment = moment(dob.trim());
-      if (dobMoment.isValid()) {
-        const dobId = yield call(EntityDataModelApi.getPropertyTypeId, PERSON_DOB_FQN);
-        updateSearchField(dobMoment.format('YYYY-MM-DD'), dobId, true);
+    if (dob.length) {
+      const dobDT = DateTime.fromISO(dob);
+      if (dobDT.isValid) {
+        updateSearchField(dobDT.toISODate(), dobPTID, true);
       }
     }
+
     const searchOptions = {
       searchFields,
       start: 0,
       maxHits: 100
     };
 
-    const peopleESId = getPersonEntitySetId(app);
+    const peopleESId = getPeopleESId(app);
     const response = yield call(SearchApi.advancedSearchEntitySetData, peopleESId, searchOptions);
 
-    yield put(searchPeople.success(id, response.hits));
+    yield put(searchPeople.success(action.id, response.hits));
   }
   catch (error) {
-    yield put(searchPeople.failure(id, error));
+    yield put(searchPeople.failure(action.id, error));
   }
   finally {
-    yield put(searchPeople.finally(id));
+    yield put(searchPeople.finally(action.id));
   }
 }
 
@@ -107,7 +107,7 @@ function* editPersonWorker(action :SequenceAction) :Generator<*, *, *> {
   try {
     yield put(editPerson.request(id));
     const entityKeyId = entity.getIn([OPENLATTICE_ID_FQN, 0], '');
-    const entitySetId = getPersonEntitySetId(app);
+    const entitySetId = getPeopleESId(app);
 
     const { propertyTypes } = yield call(EntityDataModelApi.getEntityDataModelProjection, [{
       id: entitySetId,
@@ -131,18 +131,18 @@ function* editPersonWorker(action :SequenceAction) :Generator<*, *, *> {
 
     const updates = {
       [entityKeyId]: entityWithIds.toJS()
-    }
+    };
 
     yield call(DataApi.updateEntityData, entitySetId, updates, 'Replace');
-    const person = yield call(DataApi.getEntityData, entitySetId, entityKeyId)
+    const person = yield call(DataApi.getEntityData, entitySetId, entityKeyId);
 
-    yield put(editPerson.success(id, { entityKeyId, person }))
+    yield put(editPerson.success(id, { entityKeyId, person }));
   }
   catch (error) {
     yield put(editPerson.failure(id, error));
   }
   finally {
-    yield put(editPerson.finally(id))
+    yield put(editPerson.finally(id));
   }
 }
 
