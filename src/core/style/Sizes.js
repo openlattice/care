bbe9fcef8d1a6648b1befc616144846e94a4baac
Solomon Tang <solomoncztang@gmail.com>
2019-06-24 23:52:24 -0700

    Remove comment

--- src/core/style/Sizes.js
+++ src/core/style/Sizes.js
@@ -4,7 +4,6 @@
 
 export const APP_CONTAINER_MAX_WIDTH :number = 1300;
 
-// 1020 = 960 for content + 2*30 for edges padding
 export const APP_CONTAINER_WIDTH :number = 1024;
 export const APP_CONTENT_PADDING :number = 30;
 
