2b0dbd257aa55f7cc24cde9325f21d780bd045a1
Solomon Tang <solomoncztang@gmail.com>
2019-06-25 00:15:41 -0700

    export crisisCountSkeleton

--- src/components/skeletons/index.js
+++ src/components/skeletons/index.js
@@ -1,7 +1,9 @@
 import { behaviorItemSkeleton } from './BehaviorItemSkeleton';
 import { aboutDetailSkeleton } from './AboutDetailSkeleton';
+import { crisisCountSkeleton } from './CrisisCountSkeleton';
 
 export {
   aboutDetailSkeleton,
-  behaviorItemSkeleton
+  behaviorItemSkeleton,
+  crisisCountSkeleton
 };
