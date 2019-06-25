8333d3eccaf314044c40b7c80eac759a5312b2f3
Solomon Tang <solomoncztang@gmail.com>
2019-06-24 19:14:37 -0700

    Tidy up ReportsSummary

--- src/containers/profile/premium/ReportsSummary.js
+++ src/containers/profile/premium/ReportsSummary.js
@@ -1,12 +1,15 @@
 // @flow
 import React, { PureComponent } from 'react';
-import memoizeOne from 'memoize-one';
 import { Models } from 'lattice';
 import { List, Map } from 'immutable';
 import { Card, CardSegment } from 'lattice-ui-kit';
-import { OBSERVED_BEHAVIORS_FQN } from '../../../edm/DataModelFqns';
 import PercentBarChart from '../../../components/dashboard/charts/PercentBarChart';
 import { countPropertyOccurrance } from './Utils';
+import {
+  OBSERVED_BEHAVIORS_FQN,
+  DISPATCH_REASON_FQN,
+  DISPOSITION_FQN,
+} from '../../../edm/DataModelFqns';
 
 const { FullyQualifiedName } = Models;
 
@@ -16,20 +19,7 @@ type Props = {
 
 class ReportsSummary extends PureComponent<Props> {
 
-  /*
-  [
-    {
-      name: 'behavior1',
-      count: 1
-    },
-    {
-      name: 'behavior2',
-      count: 2
-    }
-  ]
-  */
-
-  countPropertyValues = memoizeOne((reports :List, propertyTypeFqn :FullyQualifiedName) :Map => {
+  countPropertyValues = (reports :List, propertyTypeFqn :FullyQualifiedName) :Map => {
     const total = reports.count();
     return countPropertyOccurrance(reports, propertyTypeFqn)
       .sortBy(count => count, (valueA, valueB) => valueB - valueA)
@@ -38,29 +28,47 @@ class ReportsSummary extends PureComponent<Props> {
         const percent = Math.round((count / total) * 100);
         return { name, count, percent };
       });
-  });
+  }
 
   renderBehaviorChart = () => {
-
+    const { reports } = this.props;
+    const total = reports.count();
+    const data = this.countPropertyValues(reports, OBSERVED_BEHAVIORS_FQN);
+    return (
+      <CardSegment padding="sm" vertical>
+        <PercentBarChart data={data} total={total} />
+      </CardSegment>
+    );
   }
 
   renderNatureOfCrisisChart = () => {
-
+    const { reports } = this.props;
+    const total = reports.count();
+    const data = this.countPropertyValues(reports, DISPATCH_REASON_FQN);
+    return (
+      <CardSegment padding="sm" vertical>
+        <PercentBarChart data={data} total={total} />
+      </CardSegment>
+    );
   }
 
   renderDispositionChart = () => {
-
+    const { reports } = this.props;
+    const total = reports.count();
+    const data = this.countPropertyValues(reports, DISPOSITION_FQN);
+    return (
+      <CardSegment padding="sm" vertical>
+        <PercentBarChart data={data} total={total} />
+      </CardSegment>
+    );
   }
 
   render() {
-    const { reports } = this.props;
-    const data = this.countPropertyValues(reports, OBSERVED_BEHAVIORS_FQN);
-    const total = reports.count();
     return (
       <Card>
-        <CardSegment padding="sm" vertical>
-          <PercentBarChart data={data} total={total} />
-        </CardSegment>
+        { this.renderBehaviorChart() }
+        { this.renderNatureOfCrisisChart() }
+        { this.renderDispositionChart() }
       </Card>
     );
   }
