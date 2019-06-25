65ac027c28ed87deb75656f59bcf49171f6c43f0
Solomon Tang <solomoncztang@gmail.com>
2019-06-24 19:37:34 -0700

    Add incrementValueAtKey util

--- src/containers/profile/premium/Utils.js
+++ src/containers/profile/premium/Utils.js
@@ -4,24 +4,30 @@ import { List, Map } from 'immutable';
 
 const { FullyQualifiedName } = Models;
 
+const incrementValueAtKey = (mutable :Map, key :string, value :boolean) => {
+  if (value) {
+    if (mutable.has(key)) {
+      mutable.update(key, count => count + 1);
+    }
+    else {
+      mutable.set(key, 1);
+    }
+  }
+};
+
 const countPropertyOccurrance = (reports :List<Map>, propertyTypeFqn :FullyQualifiedName) => Map()
   .withMutations((mutable) => {
     reports.forEach((report) => {
       const propertyValues = report.get(propertyTypeFqn, []);
 
       propertyValues.forEach((value) => {
-        if (value) {
-          if (mutable.has(value)) {
-            mutable.update(value, count => count + 1);
-          }
-          else {
-            mutable.set(value, 1);
-          }
-        }
+        incrementValueAtKey(mutable, value, value);
       });
+
     });
   });
 
 export {
-  countPropertyOccurrance
+  countPropertyOccurrance,
+  incrementValueAtKey,
 };
