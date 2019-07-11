377fae1e3d83c3ba7c338d5a3f328bf05912c7b5
Solomon Tang <solomoncztang@gmail.com>
2019-07-10 18:02:46 -0700

    Fork getPhysicalAppearanceWorker

--- src/core/sagas/Sagas.js
+++ src/core/sagas/Sagas.js
@@ -72,6 +72,7 @@ export default function* sagas() :Generator<*, *, *> {
 
     // Profile Sagas
     fork(ProfileSagas.getPersonDataWatcher),
+    fork(ProfileSagas.getPhysicalApperanceWatcher),
     fork(ProfileSagas.getProfileReportsWatcher),
     fork(ProfileSagas.updateProfileAboutWatcher),
 
