0e8931c6e69723475b9c70764452bb465ae6923c
Solomon Tang <solomoncztang@gmail.com>
2019-07-12 19:19:47 -0700

    Renders Stepper from LUK

--- src/containers/profile/edit/EditProfileContainer.js
+++ src/containers/profile/edit/EditProfileContainer.js
@@ -1,17 +1,48 @@
+// @flow
 import React from 'react';
 // import { Switch, Route } from 'react-router-dom';
-import { ContentOuterWrapper, ContentWrapper } from '../../../components/layout';
-import StepNavBar from './StepNavBar';
+import {
+  Card,
+  CardSegment,
+  CardStack,
+  Stepper,
+} from 'lattice-ui-kit';
+import type { Match } from 'react-router-dom';
 
-const EditProfileContainer = () => (
-  <ContentOuterWrapper>
-    <ContentWrapper>
-      <StepNavBar>
-        <div>i am content 1</div>
-        <div>i am content 2</div>
-      </StepNavBar>
-    </ContentWrapper>
-  </ContentOuterWrapper>
-);
+import { ContentOuterWrapper, ContentWrapper } from '../../../components/layout';
+import NavStep from './NavStep';
+
+type Props = {
+  match :Match;
+};
+
+const EditProfileContainer = (props :Props) => {
+  const { match } = props;
+
+  return (
+    <ContentOuterWrapper>
+      <ContentWrapper>
+        <CardStack>
+          <Card>
+            <CardSegment padding="sm">
+              <Stepper>
+                <NavStep to={`${match.url}/basic-information`}>Basic Information</NavStep>
+                <NavStep to={`${match.url}/officer-safety`}>Officer Safety</NavStep>
+                <NavStep to={`${match.url}/response-plan`}>Background & Response Plan</NavStep>
+                <NavStep to={`${match.url}/contacts`}>Contacts</NavStep>
+                <NavStep to={`${match.url}/about`}>About</NavStep>
+              </Stepper>
+            </CardSegment>
+          </Card>
+          <Card>
+            <CardSegment padding="sm" vertical>
+              content
+            </CardSegment>
+          </Card>
+        </CardStack>
+      </ContentWrapper>
+    </ContentOuterWrapper>
+  );
+};
 
 export default EditProfileContainer;
