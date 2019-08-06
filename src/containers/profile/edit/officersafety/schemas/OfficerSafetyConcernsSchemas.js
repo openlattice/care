2ab14abc9ec1cf645fee7ed2ef76cf2ee75e0f3a
Solomon Tang <solomoncztang@gmail.com>
2019-08-05 19:22:05 -0700

    add OfficerSafetyconcernsSchemas

---
+++ src/containers/profile/edit/officersafety/schemas/OfficerSafetyConcernsSchemas.js
@@ -0,0 +1,70 @@
+import { DataProcessingUtils } from 'lattice-fabricate';
+import { APP_TYPES_FQNS } from '../../../../../shared/Consts';
+import {
+  DESCRIPTION_FQN,
+  CATEGORY_FQN
+} from '../../../../../edm/DataModelFqns';
+
+import { SAFETY_TYPES } from './constants';
+
+const { getEntityAddressKey, getPageSectionKey } = DataProcessingUtils;
+const { OFFICER_SAFETY_CONCERNS_FQN } = APP_TYPES_FQNS;
+
+const schema = {
+  definitions: {
+    officerSafetyCaution: {
+      type: 'object',
+      properties: {
+        [getEntityAddressKey(-1, OFFICER_SAFETY_CONCERNS_FQN, CATEGORY_FQN)]: {
+          type: 'string',
+          title: 'Safety type',
+          enum: SAFETY_TYPES
+        },
+        [getEntityAddressKey(-1, OFFICER_SAFETY_CONCERNS_FQN, DESCRIPTION_FQN)]: {
+          type: 'string',
+          title: 'Description',
+        }
+      }
+    }
+  },
+  type: 'object',
+  title: 'Officer Safety',
+  properties: {
+    [getPageSectionKey(1, 1)]: {
+      type: 'array',
+      title: 'Officer Safety Concerns',
+      items: {
+        $ref: '#/definitions/officerSafetyCaution'
+      },
+      default: [
+        {
+          [getEntityAddressKey(-1, OFFICER_SAFETY_CONCERNS_FQN, CATEGORY_FQN)]: undefined,
+          [getEntityAddressKey(-1, OFFICER_SAFETY_CONCERNS_FQN, DESCRIPTION_FQN)]: undefined,
+        }
+      ]
+    }
+  }
+};
+
+const uiSchema = {
+  [getPageSectionKey(1, 1)]: {
+    classNames: 'column-span-12',
+    'ui:options': {
+      addButtonText: '+ Add Officer Safety Caution',
+      addActionKey: 'addOfficerSafetyCaution',
+      orderable: false
+    },
+    items: {
+      classNames: 'grid-container',
+      [getEntityAddressKey(-1, OFFICER_SAFETY_CONCERNS_FQN, CATEGORY_FQN)]: {
+        classNames: 'column-span-12',
+        'ui:widget': 'radio'
+      },
+      [getEntityAddressKey(-1, OFFICER_SAFETY_CONCERNS_FQN, DESCRIPTION_FQN)]: {
+        classNames: 'column-span-12'
+      },
+    }
+  }
+};
+
+export { schema, uiSchema };
