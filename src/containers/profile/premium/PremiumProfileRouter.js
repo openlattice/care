5867518cb5a5832465bcbca59aefcc6b90795c21
Solomon Tang <solomoncztang@gmail.com>
2019-08-07 11:00:01 -0700

    Redirect urls with trailing slash

--- src/containers/profile/premium/PremiumProfileRouter.js
+++ src/containers/profile/premium/PremiumProfileRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { PROFILE_PATH } from '../../../core/router/Routes';
 
 import PremiumProfileContainer from './PremiumProfileContainer';
@@ -7,6 +7,7 @@ import EditProfileContainer from '../edit/EditProfileContainer';
 
 const PremiumProfileRouter = () => (
   <Switch>
+    <Redirect strict exact from={`${PROFILE_PATH}/`} to={PROFILE_PATH} />
     <Route exact path={PROFILE_PATH} component={PremiumProfileContainer} />
     <Route component={EditProfileContainer} />
   </Switch>
