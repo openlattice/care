ac51edfc4a5f5c340d54d53a3ad51b35575199c3
Solomon Tang <solomoncztang@gmail.com>
2019-08-07 17:15:25 -0700

    address form styles

--- src/containers/profile/edit/basicinformation/schemas/AddressSchemas.js
+++ src/containers/profile/edit/basicinformation/schemas/AddressSchemas.js
@@ -55,27 +55,27 @@ const schema = {
 
 const uiSchema = {
   [getPageSectionKey(1, 1)]: {
-    classNames: 'column-span-4 grid-container',
+    classNames: 'column-span-12 grid-container',
     'ui:options': {
       editable: true
     },
     [getEntityAddressKey(0, LOCATION_FQN, LOCATION_NAME_FQN)]: {
-      classNames: 'column-span-12'
+      classNames: 'column-span-6'
     },
     [getEntityAddressKey(0, LOCATION_FQN, LOCATION_STREET_FQN)]: {
-      classNames: 'column-span-12'
+      classNames: 'column-span-8'
     },
     [getEntityAddressKey(0, LOCATION_FQN, LOCATION_ADDRESS_LINE_2_FQN)]: {
-      classNames: 'column-span-12'
+      classNames: 'column-span-8'
     },
     [getEntityAddressKey(0, LOCATION_FQN, LOCATION_CITY_FQN)]: {
-      classNames: 'column-span-12'
+      classNames: 'column-span-6'
     },
     [getEntityAddressKey(0, LOCATION_FQN, LOCATION_STATE_FQN)]: {
-      classNames: 'column-span-12'
+      classNames: 'column-span-3'
     },
     [getEntityAddressKey(0, LOCATION_FQN, LOCATION_ZIP_FQN)]: {
-      classNames: 'column-span-12'
+      classNames: 'column-span-3'
     },
   }
 };
