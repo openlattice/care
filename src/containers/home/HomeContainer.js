d4073d7e6323bd2b7094e4a546a57851a9ca3cfd
Solomon Tang <solomoncztang@gmail.com>
2019-08-15 18:45:01 -0700

    use Card over StyledCard

--- src/containers/home/HomeContainer.js
+++ src/containers/home/HomeContainer.js
@@ -5,15 +5,18 @@
 import React from 'react';
 
 import styled from 'styled-components';
+import { Map } from 'immutable';
 import { faAngleRight, faFileAlt } from '@fortawesome/pro-light-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { NavLink } from 'react-router-dom';
+import { Card, CardSegment, Colors } from 'lattice-ui-kit';
 
-import StyledCard from '../../components/cards/StyledCard';
 import * as Routes from '../../core/router/Routes';
 import { MEDIA_QUERY_MD } from '../../core/style/Sizes';
+
+const { NEUTRALS } = Colors;
 
 /*
  * styled components
@@ -40,30 +43,22 @@ const Content = styled.div`
 `;
 
 const StyledNavLink = styled(NavLink)`
-  color: #113355;
+  color: ${NEUTRALS[0]};
   text-decoration: none;
   &:hover {
     cursor: pointer;
-    text-decoration: none;
-    > div {
-      border-color: #95aabf;
-    }
-  }
-
-  &:not(:first-child) {
-    margin-top: 25px;
   }
 `;
 
-const StyledNavCard = styled(StyledCard)`
-  display: flex;
-  flex-direction: row;
+const StyledNavCard = styled(CardSegment)`
   justify-content: space-between;
 `;
 
 const LinkTextWrapper = styled.div`
   align-items: center;
   display: flex;
+  font-size: 14px;
+  padding: 0 15px;
 `;
 
 const LinkText = styled.span`
@@ -110,15 +105,17 @@ const LinkText = styled.span`
 const HomeContainer = () => (
   <ContainerWrapper>
     <Content>
-      <StyledNavLink to={Routes.CRISIS_PATH}>
-        <StyledNavCard>
-          <LinkTextWrapper>
-            <FontAwesomeIcon icon={faFileAlt} size="2x" />
-            <LinkText>Crisis Report</LinkText>
-          </LinkTextWrapper>
-          <FontAwesomeIcon icon={faAngleRight} size="2x" />
-        </StyledNavCard>
-      </StyledNavLink>
+      <Card onClick={() => {}}>
+        <StyledNavLink to={Routes.CRISIS_PATH}>
+          <StyledNavCard>
+            <LinkTextWrapper>
+              <FontAwesomeIcon icon={faFileAlt} size="2x" />
+              <LinkText>Crisis Report</LinkText>
+            </LinkTextWrapper>
+            <FontAwesomeIcon icon={faAngleRight} size="2x" />
+          </StyledNavCard>
+        </StyledNavLink>
+      </Card>
     </Content>
   </ContainerWrapper>
 );
