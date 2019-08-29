74e73a94c7ad0d305076e4f3ed175614804717af
Solomon Tang <solomoncztang@gmail.com>
2019-08-29 14:57:47 -0700

    Surface OfficerSafetyConcernsList on profile

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
+import { Colors } from 'lattice-ui-kit';
 
 import { APP_CONTAINER_WIDTH } from '../../core/style/Sizes';
-import { bulletsSkeleton } from '../skeletons';
+import { bulletsSkeleton, behaviorItemSkeleton } from '../skeletons';
+
+const { NEUTRALS } = Colors;
 
 export const ContentOuterWrapper = styled.div`
   display: flex;
@@ -23,4 +26,20 @@ export const ContentWrapper = styled.div`
 export const UL = styled.ul`
   padding-inline-start: 20px;
   ${props => (props.isLoading ? bulletsSkeleton : null)};
+`;
+
+export const DashedList = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+
+  > div {
+    border-bottom: 1px dashed ${NEUTRALS[4]};
+  }
+
+  > div:last-child {
+    border-bottom: 0;
+  }
+
+  ${props => (props.isLoading ? behaviorItemSkeleton : null)};
 `;
