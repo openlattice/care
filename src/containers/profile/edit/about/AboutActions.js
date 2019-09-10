a7328ad2e7e263e389b6e3e8158a77bf70083dcc
Solomon Tang <solomoncztang@gmail.com>
2019-09-09 19:35:19 -0700

    Add new actions

--- src/containers/profile/edit/about/AboutActions.js
+++ src/containers/profile/edit/about/AboutActions.js
@@ -10,11 +10,16 @@ const getResponsibleUser = newRequestSequence(GET_RESPONSIBLE_USER);
 const UPDATE_RESPONSIBLE_USER :'UPDATE_RESPONSIBLE_USER' = 'UPDATE_RESPONSIBLE_USER';
 const updateResponsibleUser = newRequestSequence(UPDATE_RESPONSIBLE_USER);
 
+const GET_RESPONSIBLE_USER_OPTIONS :'GET_RESPONSIBLE_USER_OPTIONS' = 'GET_RESPONSIBLE_USER_OPTIONS';
+const getResponsibleUserOptions = newRequestSequence(GET_RESPONSIBLE_USER_OPTIONS);
+
 export {
   GET_RESPONSIBLE_USER,
+  GET_RESPONSIBLE_USER_OPTIONS,
   SUBMIT_RESPONSIBLE_USER,
   UPDATE_RESPONSIBLE_USER,
   getResponsibleUser,
+  getResponsibleUserOptions,
   submitResponsibleUser,
   updateResponsibleUser,
 };
