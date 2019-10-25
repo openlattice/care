e4149822257bf01b696d0619af8c72e88f43e71e
Solomon Tang <solomoncztang@gmail.com>
2019-10-24 17:20:20 -0700

    Remove unused variables

--- src/containers/home/HomeContainer.js
+++ src/containers/home/HomeContainer.js
@@ -5,11 +5,8 @@
 import React from 'react';
 
 import styled from 'styled-components';
-import { Map } from 'immutable';
 import { faAngleRight, faFileAlt } from '@fortawesome/pro-light-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
 import { NavLink } from 'react-router-dom';
 import { Card, CardSegment, Colors } from 'lattice-ui-kit';
 
@@ -71,37 +68,6 @@ const LinkText = styled.span`
   }
 `;
 
-// HACK
-/* const CRISIS_REPORT_ORG_IDS = [
-  // WASPC
-  '8c732544-4c51-4129-b326-c87c83ac20a7',
-  '9a582e49-2607-4817-9157-c0c484706297',
-  'b59565ac-251b-476e-8340-97aa2d9a05ab',
-  '11351e88-58e5-422c-b0b8-72171d879e7f',
-  '820caca5-55c1-40c9-a537-2b56b3574c78',
-  'eb27c39b-8c3e-453e-b20e-f3893df8fa3f',
-  'c2f5deaf-4acd-4d16-8b43-5adecdef9a99',
-  'b39e5188-8bef-42b7-912c-5ca30c42f118',
-  '243d713b-cb34-4eab-998a-492cfb47cd43',
-  'c83e7cb9-9a7e-46e0-9bf2-62a0789d1942',
-  '1197e2b4-ed0c-4656-9b00-604aeef11287',
-  '6b8b9830-794f-44be-be04-1a8e4bfde053',
-  '977dd0e1-4dee-4a5a-88ec-b01fd0937b34',
-  '6712201d-9fbd-49e1-a4bd-b8a88651ffa7',
-  '42802c67-7ff0-4ff0-8d9d-f96f0c8ca942',
-
-  // JOHNSON COUNTY
-  'aeba1be8-5d72-4f46-b0a9-5319bfb40faa',
-
-  // KCSO
-  'aa07442f-4835-4d95-83ed-a7f28d1a1584',
-  'aa7e90ba-b4f2-41ea-9f5b-7f220d605afa',
-  'c957fb77-940e-47cf-a7bd-e43ce2ec3e13',
-  '67089f2e-48be-41e9-87b2-2269de47f101',
-  'fd85f4e9-419c-4af9-8195-dc3a883bbed8',
-  '7eebd1bf-71af-4339-8b2f-fe4f89d18393'
-]; */
-
 const HomeContainer = () => (
   <ContainerWrapper>
     <Content>
@@ -120,14 +86,5 @@ const HomeContainer = () => (
   </ContainerWrapper>
 );
 
-function mapStateToProps(state :Map<*, *>) :Object {
-
-  return {
-    orgId: state.getIn(['app', 'selectedOrganizationId'], '')
-  };
-}
-
 // $FlowFixMe
-export default withRouter(
-  connect(mapStateToProps, null)(HomeContainer)
-);
+export default HomeContainer;
