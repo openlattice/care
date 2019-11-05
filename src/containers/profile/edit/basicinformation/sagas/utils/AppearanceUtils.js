f7e0c046e142d93b96ce04fb3592b0c88f258d7a
Solomon Tang <solomoncztang@gmail.com>
2019-11-04 18:35:13 -0800

    Remove indentifying characteristics from AppearanceForm

--- src/containers/profile/edit/basicinformation/sagas/utils/AppearanceUtils.js
+++ src/containers/profile/edit/basicinformation/sagas/utils/AppearanceUtils.js
@@ -6,17 +6,11 @@ import { APP_TYPES_FQNS } from '../../../../../../shared/Consts';
 import * as FQN from '../../../../../../edm/DataModelFqns';
 import { isValidUuid } from '../../../../../../utils/Utils';
 
-const {
-  PHYSICAL_APPEARANCE_FQN,
-  IDENTIFYING_CHARACTERISTICS_FQN,
-} = APP_TYPES_FQNS;
+const { PHYSICAL_APPEARANCE_FQN } = APP_TYPES_FQNS;
 
 const { getPageSectionKey } = DataProcessingUtils;
 
-const constructFormData = (
-  appearance :Map,
-  marks :Map,
-) => {
+const constructFormData = (appearance :Map) => {
 
   const appearanceProperties = [
     FQN.EYE_COLOR_FQN,
@@ -25,8 +19,6 @@ const constructFormData = (
     FQN.WEIGHT_FQN,
   ];
 
-  const marksProperties = [FQN.DESCRIPTION_FQN];
-
   const appearanceFormData = getFormDataFromEntity(
     appearance,
     PHYSICAL_APPEARANCE_FQN,
@@ -34,29 +26,17 @@ const constructFormData = (
     0
   );
 
-  const marksFormData = getFormDataFromEntity(
-    marks,
-    IDENTIFYING_CHARACTERISTICS_FQN,
-    marksProperties,
-    0
-  );
-
   return Map().withMutations((mutable) => {
     if (!appearanceFormData.isEmpty()) mutable.mergeIn([getPageSectionKey(1, 1)], appearanceFormData);
-    if (!marksFormData.isEmpty()) mutable.mergeIn([getPageSectionKey(1, 1)], marksFormData);
   });
 
 };
 
-const constructEntityIndexToIdMap = (
-  apperanceEKID :UUID,
-  marksEKID :UUID,
-) => {
+const constructEntityIndexToIdMap = (apperanceEKID :UUID) => {
   const entityIndexToIdMap = Map().withMutations((mutable) => {
     if (isValidUuid(apperanceEKID)) {
       mutable.setIn([PHYSICAL_APPEARANCE_FQN.toString(), 0], apperanceEKID);
     }
-    if (isValidUuid(marksEKID)) mutable.setIn([IDENTIFYING_CHARACTERISTICS_FQN.toString(), 0], marksEKID);
   });
 
   return entityIndexToIdMap;
