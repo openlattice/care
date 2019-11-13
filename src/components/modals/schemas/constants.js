06e3a501f7e771f6f35418b16056638d5a592c2b
Solomon Tang <solomoncztang@gmail.com>
2019-11-13 15:46:02 -0800

    Add request changes constants

---
+++ src/components/modals/schemas/constants.js
@@ -0,0 +1,20 @@
+const PRIORITIES = [
+  'Highest',
+  'High',
+  'Medium',
+  'Low',
+  'Lowest'
+];
+
+const COMPONENTS = [
+  'Basic Information',
+  'Officer Safety',
+  'Response Plan',
+  'Contacts',
+  'About'
+];
+
+export {
+  COMPONENTS,
+  PRIORITIES
+};
