74ee6007780f42eb861900529ce8644256432c3d
Solomon Tang <solomoncztang@gmail.com>
2020-01-09 14:48:23 -0800

    Move route constants to LongBeachRouter

--- src/longbeach/LongBeachRouter.js
+++ src/longbeach/LongBeachRouter.js
@@ -1,16 +1,17 @@
+// @flow
 import React from 'react';
 
 import { Redirect, Route, Switch } from 'react-router-dom';
 
 import LongBeachHome from './LongBeachHome';
 
-import {
-  HOME_PATH,
-  LOCATION_PATH,
-  PEOPLE_PATH,
-  PROFILE_PATH,
-  PROVIDER_PATH,
-} from '../core/router/Routes';
+/* <===== BEGIN LONG BEACH HACK =====> */
+export const HOME_PATH :string = '/home';
+export const PEOPLE_PATH :string = '/people';
+export const LOCATION_PATH :string = '/location';
+export const PROVIDER_PATH :string = '/provider';
+export const PROFILE_PATH :string = '/profile';
+/* <===== END LONG BEACH HACK =====> */
 
 const LongBeachRouter = () => (
   <Switch>
