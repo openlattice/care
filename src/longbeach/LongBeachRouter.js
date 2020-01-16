7cd1b79aa6d1b7248a7049251598ee9cf4885827
Solomon Tang <solomoncztang@gmail.com>
2020-01-16 12:33:08 -0800

    pull hard coded provider information

--- src/longbeach/LongBeachRouter.js
+++ src/longbeach/LongBeachRouter.js
@@ -7,6 +7,7 @@ import LongBeachHome from './LongBeachHome';
 import LongBeachLocationsContainer from './location/LongBeachLocationsContainer';
 import LongBeachPeopleContainer from './people/LongBeachPeopleContainer';
 import LongBeachProfileContainer from './profile/LongBeachProfileContainer';
+import LongBeachProviderContainer from './provider/LongBeachProviderContainer';
 import {
   HOME_PATH,
   LOCATION_PATH,
@@ -20,7 +21,7 @@ const LongBeachRouter = () => (
     <Route exact strict path={HOME_PATH} component={LongBeachHome} />
     <Route path={LOCATION_PATH} component={LongBeachLocationsContainer} />
     <Route path={PEOPLE_PATH} component={LongBeachPeopleContainer} />
-    <Route path={PROVIDER_PATH} component={LongBeachHome} />
+    <Route path={PROVIDER_PATH} component={LongBeachProviderContainer} />
     <Route strict path={PROFILE_VIEW_PATH} component={LongBeachProfileContainer} />
     <Redirect to={HOME_PATH} />
   </Switch>
