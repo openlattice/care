f941186cc8a936be2a8644c5e2737528ec08deb9
Solomon Tang <solomoncztang@gmail.com>
2020-02-19 14:43:43 -0800

    Sanitize store

--- src/core/redux/ReduxStore.js
+++ src/core/redux/ReduxStore.js
@@ -2,15 +2,19 @@
  * @flow
  */
 
-import Immutable from 'immutable';
 import createSagaMiddleware from '@redux-saga/core';
 import { routerMiddleware } from 'connected-react-router/immutable';
+import { Map } from 'immutable';
+import { EntityDataModelApiActions } from 'lattice-sagas';
 import { applyMiddleware, compose, createStore } from 'redux';
 
-import sagas from '../sagas/Sagas';
 import reduxReducer from './ReduxReducer';
+
+import sagas from '../sagas/Sagas';
 import trackingHandlers from '../tracking/google/trackinghandlers';
 import trackingMiddleware from '../tracking/TrackingMiddleware';
+
+const { getAllPropertyTypes, getEntityDataModelProjection } = EntityDataModelApiActions;
 
 export default function initializeReduxStore(routerHistory :any) :Object {
 
@@ -26,18 +30,33 @@ export default function initializeReduxStore(routerHistory :any) :Object {
     applyMiddleware(...reduxMiddlewares)
   ];
 
+  const actionSanitizer = (action :Object) :Object => {
+    switch (action.type) {
+      case getAllPropertyTypes.SUCCESS:
+      case getEntityDataModelProjection.SUCCESS:
+        return { ...action, value: 'SANITIZED: Remove actionSanitizer from enhancers to view.' };
+      default:
+        return action;
+    }
+  };
+
+  const stateSanitizer = (state :Map) :Map => state
+    .set('edm', 'SANITIZED: Remove stateSanitizer from enhancers to view.');
+
   /* eslint-disable no-underscore-dangle */
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      actionSanitizer,
       maxAge: 100,
-      serialize: true
+      stateSanitizer,
+      // serialize: true
     })
     : compose;
   /* eslint-enable */
 
   const reduxStore = createStore(
     reduxReducer(routerHistory),
-    Immutable.Map(),
+    Map(),
     composeEnhancers(...reduxEnhancers)
   );
 
