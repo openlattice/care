0aecb11abd42a67c53417278445f1a9ddf41814e
Drew <drew@openlattice.com>
2020-04-18 15:29:02 -0400

    add babel-plugin-transform-imports and configuration to babel.config.js to reduce fontawesome size in final dist

--- config/babel/babel.config.js
+++ config/babel/babel.config.js
@@ -4,6 +4,28 @@ module.exports = {
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-transform-runtime',
     'babel-plugin-styled-components',
+    ['babel-plugin-transform-imports', {
+      '@fortawesome/pro-duotone-svg-icons': {
+        transform: (importName) => `@fortawesome/pro-duotone-svg-icons/${importName}`,
+        preventFullImport: true,
+        skipDefaultConversion: true
+      },
+      '@fortawesome/pro-light-svg-icons': {
+        transform: (importName) => `@fortawesome/pro-light-svg-icons/${importName}`,
+        preventFullImport: true,
+        skipDefaultConversion: true
+      },
+      '@fortawesome/pro-regular-svg-icons': {
+        transform: (importName) => `@fortawesome/pro-regular-svg-icons/${importName}`,
+        preventFullImport: true,
+        skipDefaultConversion: true
+      },
+      '@fortawesome/pro-solid-svg-icons': {
+        transform: (importName) => `@fortawesome/pro-solid-svg-icons/${importName}`,
+        preventFullImport: true,
+        skipDefaultConversion: true
+      },
+    }]
   ],
   presets: [
     '@babel/preset-env',
