232890fc9e407cca2df6385e02bd84afa51817c8
Solomon Tang <solomoncztang@gmail.com>
2020-05-05 05:38:49 -0700

    Add follow up containers

---
+++ src/containers/reports/crisis/FollowupReportContainer.js
@@ -0,0 +1,144 @@
+// @flow
+import React, { useCallback, useEffect, useMemo } from 'react';
+
+import { Map } from 'immutable';
+import { Form } from 'lattice-fabricate';
+import {
+  Breadcrumbs,
+  Card,
+  CardStack,
+  Spinner,
+} from 'lattice-ui-kit';
+import { useDispatch, useSelector } from 'react-redux';
+import { useRouteMatch } from 'react-router';
+import { RequestStates } from 'redux-reqseq';
+
+import {
+  addOptionalCrisisReportContent,
+  deleteCrisisReportContent,
+  getCrisisReportV2,
+  updateCrisisReport
+} from './CrisisActions';
+import { v2 } from './schemas';
+
+import BlameCard from '../shared/BlameCard';
+import * as FQN from '../../../edm/DataModelFqns';
+import { BreadcrumbItem, BreadcrumbLink } from '../../../components/breadcrumbs';
+import { useAuthorization } from '../../../components/hooks';
+import { ContentOuterWrapper, ContentWrapper } from '../../../components/layout';
+import {
+  FOLLOW_UP_REPORT_PATH,
+  PROFILE_ID_PATH,
+  PROFILE_VIEW_PATH,
+  REPORT_ID_PARAM,
+} from '../../../core/router/Routes';
+import { getAuthorization } from '../../../core/sagas/authorize/AuthorizeActions';
+import { APP_TYPES_FQNS } from '../../../shared/Consts';
+import { getEntityKeyId } from '../../../utils/DataUtils';
+import { getFirstLastFromPerson } from '../../../utils/PersonUtils';
+import { generateReviewSchema } from '../../../utils/SchemaUtils';
+
+const { FOLLOW_UP_REPORT_FQN } = APP_TYPES_FQNS;
+
+const FollowupReportContainer = () => {
+  const dispatch = useDispatch();
+  const match = useRouteMatch(FOLLOW_UP_REPORT_PATH);
+
+  const dispatchGetAuthorization = useCallback(() => {
+    dispatch(getAuthorization());
+  }, [dispatch]);
+
+  const [isAuthorized] = useAuthorization('profile', dispatchGetAuthorization);
+
+  const schemaVersion = v2.followup;
+
+  const reviewSchemas = useMemo(
+    () => generateReviewSchema(schemaVersion.schemas, schemaVersion.uiSchemas, !isAuthorized),
+    [schemaVersion, isAuthorized]
+  );
+
+  const entityIndexToIdMap = useSelector((store) => store.getIn(['crisisReport', 'entityIndexToIdMap']));
+  const entitySetIds = useSelector((store) => store.getIn(['app', 'selectedOrgEntitySetIds'], Map()));
+  const formData = useSelector((store) => store.getIn(['crisisReport', 'formData']));
+  const fetchState = useSelector((store) => store.getIn(['crisisReport', 'fetchState']));
+  const propertyTypeIds = useSelector((store) => store.getIn(['edm', 'fqnToIdMap'], Map()));
+  const reporterData = useSelector((store) => store.getIn(['crisisReport', 'reporterData']));
+  const reportData = useSelector((store) => store.getIn(['crisisReport', 'reportData']));
+  const subjectData = useSelector((store) => store.getIn(['crisisReport', 'subjectData']));
+
+  const { [REPORT_ID_PARAM]: reportId } = match.params;
+
+  useEffect(() => {
+    dispatch(getCrisisReportV2({
+      reportEKID: reportId,
+      reportFQN: FOLLOW_UP_REPORT_FQN,
+      reviewSchema: reviewSchemas.schema
+    }));
+  }, [dispatch, reportId, reviewSchemas]);
+
+  if (fetchState === RequestStates.PENDING) {
+    return <Spinner size="3x" />;
+  }
+
+  const handleUpdateCrisisReport = (params) => {
+    dispatch(updateCrisisReport({
+      ...params,
+      entityIndexToIdMap,
+    }));
+  };
+
+  const handleDeleteCrisisReportContent = (params) => {
+    dispatch(deleteCrisisReportContent(params));
+  };
+
+  const handleAddOptionalContent = (params) => {
+    const existingEKIDs = {
+      [FOLLOW_UP_REPORT_FQN]: reportId,
+    };
+
+    dispatch(addOptionalCrisisReportContent({
+      ...params,
+      existingEKIDs,
+      schema: reviewSchemas.schema,
+      reportFQN: FOLLOW_UP_REPORT_FQN
+    }));
+  };
+
+  const name = getFirstLastFromPerson(subjectData);
+  const formContext = {
+    addActions: {
+      addOptional: handleAddOptionalContent
+    },
+    deleteAction: handleDeleteCrisisReportContent,
+    editAction: handleUpdateCrisisReport,
+    entityIndexToIdMap,
+    entitySetIds,
+    propertyTypeIds,
+  };
+  const subjectEKID = getEntityKeyId(subjectData);
+  const profilePath = PROFILE_VIEW_PATH.replace(PROFILE_ID_PATH, subjectEKID);
+
+  return (
+    <ContentOuterWrapper>
+      <ContentWrapper>
+        <CardStack>
+          <Breadcrumbs>
+            <BreadcrumbLink to={profilePath}>{name}</BreadcrumbLink>
+            <BreadcrumbItem>{reportData.getIn([FQN.TYPE_FQN, 0], 'Report')}</BreadcrumbItem>
+          </Breadcrumbs>
+          <BlameCard reporterData={reporterData} />
+          <Card>
+            <Form
+                disabled
+                formContext={formContext}
+                formData={formData.toJS()}
+                schema={reviewSchemas.schema}
+                uiSchema={reviewSchemas.uiSchema} />
+          </Card>
+        </CardStack>
+      </ContentWrapper>
+    </ContentOuterWrapper>
+  );
+};
+
+export default FollowupReportContainer;
---
+++ src/containers/reports/crisis/NewFollowupReport.js
@@ -0,0 +1,134 @@
+// @flow
+import React, { useEffect, useMemo } from 'react';
+
+import styled from 'styled-components';
+import { Map } from 'immutable';
+import { Form, Paged } from 'lattice-fabricate';
+import {
+  Button,
+  Card,
+} from 'lattice-ui-kit';
+import { useDispatch, useSelector } from 'react-redux';
+import { RequestStates } from 'redux-reqseq';
+
+import { clearCrisisReport, submitCrisisReportV2 } from './CrisisActions';
+import { v2 } from './schemas';
+import { FOLLOW_UP_REPORT } from './schemas/constants';
+
+import SuccessSplash from '../shared/SuccessSplash';
+import { APP_TYPES_FQNS } from '../../../shared/Consts';
+import { generateReviewSchema } from '../../../utils/SchemaUtils';
+
+const { FOLLOW_UP_REPORT_FQN } = APP_TYPES_FQNS;
+
+const ActionRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 30px 30px 30px;
+`;
+
+type Props = {
+  incident :Map;
+  pageRef :{ current :HTMLDivElement | null };
+  selectedPerson :Map;
+};
+
+const NewFollowupReport = ({ incident, pageRef, selectedPerson } :Props) => {
+  const dispatch = useDispatch();
+  const submitState = useSelector((store) => store.getIn(['crisisReport', 'submitState']));
+
+  const schemaVersion = v2.followup;
+  const { schemas, uiSchemas } = schemaVersion;
+
+  const reviewSchemas = useMemo(
+    () => generateReviewSchema(schemaVersion.schemas, schemaVersion.uiSchemas, true),
+    [schemaVersion]
+  );
+
+  const getVersionSubmit = (formData :Object) => () => dispatch(submitCrisisReportV2({
+    formData,
+    incident,
+    reportFQN: FOLLOW_UP_REPORT_FQN,
+    selectedPerson,
+  }));
+
+  useEffect(() => () => dispatch(clearCrisisReport()), [dispatch]);
+
+  const isLoading = submitState === RequestStates.PENDING;
+
+  if (submitState === RequestStates.SUCCESS) {
+    return (
+      <SuccessSplash reportType={FOLLOW_UP_REPORT} selectedPerson={selectedPerson} />
+    );
+  }
+
+  return (
+    <Card>
+      <Paged
+          render={({
+            formRef,
+            pagedData,
+            page,
+            onBack,
+            onNext,
+            validateAndSubmit,
+          }) => {
+            const totalPages = schemas.length + 1;
+            const isReviewPage = page === totalPages - 1;
+
+            const validate = isReviewPage
+              ? getVersionSubmit(pagedData)
+              : validateAndSubmit;
+
+            const scrollToContentTop = () => {
+              if (pageRef.current) {
+                pageRef.current.scrollIntoView({
+                  behavior: 'smooth'
+                });
+              }
+            };
+
+            const handleNext = () => {
+              scrollToContentTop();
+              onNext();
+            };
+
+            const handleBack = () => {
+              scrollToContentTop();
+              onBack();
+            };
+
+            return (
+              <>
+                <Form
+                    disabled={isReviewPage}
+                    formData={pagedData}
+                    hideSubmit
+                    onSubmit={handleNext}
+                    ref={formRef}
+                    schema={isReviewPage ? reviewSchemas.schema : schemas[page]}
+                    uiSchema={isReviewPage ? reviewSchemas.uiSchema : uiSchemas[page]} />
+                <ActionRow>
+                  <Button
+                      disabled={!(page > 0)}
+                      onClick={handleBack}>
+                    Back
+                  </Button>
+                  <span>{`${page + 1} of ${totalPages}`}</span>
+                  <Button
+                      isLoading={isLoading}
+                      mode="primary"
+                      onClick={validate}>
+                    { isReviewPage ? 'Submit' : 'Next' }
+                  </Button>
+                </ActionRow>
+              </>
+            );
+          }} />
+    </Card>
+  );
+
+};
+
+export default NewFollowupReport;
