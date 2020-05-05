6d70b540cf7452e68bb6d3aa4f884b3477d5df68
Solomon Tang <solomoncztang@gmail.com>
2020-05-05 05:42:27 -0700

    replace banner with breadcrumbs

--- src/containers/reports/symptoms/NewSymptomsReportContainer.js
+++ src/containers/reports/symptoms/NewSymptomsReportContainer.js
@@ -4,6 +4,7 @@ import React, { useCallback, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { Map } from 'immutable';
 import {
+  Breadcrumbs,
   Button,
   Card,
   CardStack,
@@ -18,11 +19,13 @@ import LastContactWith from './LastContactWith';
 import NewSymptomsReport from './NewSymptomsReport';
 import { clearSymptomsReport } from './SymptomsReportActions';
 
-import ProfileBanner from '../../profile/ProfileBanner';
 import SuccessSplash from '../shared/SuccessSplash';
+import { BreadcrumbItem, BreadcrumbLink } from '../../../components/breadcrumbs';
 import { usePosition } from '../../../components/hooks';
 import { ContentOuterWrapper, ContentWrapper } from '../../../components/layout';
-import { HOME_PATH } from '../../../core/router/Routes';
+import { HOME_PATH, PROFILE_ID_PATH, PROFILE_VIEW_PATH } from '../../../core/router/Routes';
+import { getEntityKeyId } from '../../../utils/DataUtils';
+import { getFirstLastFromPerson } from '../../../utils/PersonUtils';
 
 const ActionRow = styled.div`
   display: flex;
@@ -51,6 +54,10 @@ const NewSymptomsReportContainer = () => {
   const { state: selectedPerson = Map() } = location;
   if (!Map.isMap(selectedPerson) || selectedPerson.isEmpty()) return <Redirect to={HOME_PATH} />;
 
+  const personEKID = getEntityKeyId(selectedPerson);
+  const profilePath = PROFILE_VIEW_PATH.replace(PROFILE_ID_PATH, personEKID);
+  const name = getFirstLastFromPerson(selectedPerson);
+
   const content = submitSuccess
     ? (
       <CardStack>
@@ -78,8 +85,11 @@ const NewSymptomsReportContainer = () => {
 
   return (
     <ContentOuterWrapper>
-      <ProfileBanner selectedPerson={selectedPerson} />
       <ContentWrapper>
+        <Breadcrumbs>
+          <BreadcrumbLink to={profilePath}>{name}</BreadcrumbLink>
+          <BreadcrumbItem>New Symptoms Report</BreadcrumbItem>
+        </Breadcrumbs>
         <CardStack>
           {
             isLoading
