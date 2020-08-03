205fc123b8bfbcc86c59326b790b10702aeda2b6
Solomon Tang <solomoncztang@gmail.com>
2020-08-03 08:08:54 -0700

    /helpline

--- src/containers/profile/helpline/HelplineContainer.js
+++ src/containers/profile/helpline/HelplineContainer.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { ProfileContainer } from '@lattice-works/lattice-helpline-center';
 import { useSelector } from 'react-redux';
 
+import { usePeopleRoute } from '../../../components/hooks';
+import { ContentOuterWrapper, ContentWrapper } from '../../../components/layout';
+
 const HelplineContainer = () => {
   const organizationId = useSelector((state) => state.getIn(['app', 'selectedOrganizationId']));
+  console.log(organizationId);
+  const [personId, setPersonId] = useState('');
+  usePeopleRoute(setPersonId);
+
+  console.log(personId);
   return (
-    <ProfileContainer organizationId={organizationId} />
+    <ContentOuterWrapper>
+      <ContentWrapper>
+        <ProfileContainer organizationId={organizationId} personId={personId} />
+      </ContentWrapper>
+    </ContentOuterWrapper>
   )
 };
 
--- src/containers/profile/premium/PremiumProfileRouter.js
+++ src/containers/profile/premium/PremiumProfileRouter.js
@@ -1,13 +1,13 @@
 // @flow
 import React, { useEffect } from 'react';
 
-import { ProfileContainer } from '@lattice-works/lattice-helpline-center';
 import { useDispatch } from 'react-redux';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
 import PremiumProfileContainer from './PremiumProfileContainer';
 
 import EditProfileContainer from '../edit/EditProfileContainer';
+import HelplineContainer from '../helpline/HelplineContainer';
 import { PROFILE_EDIT_PATH, PROFILE_PATH, PROFILE_VIEW_PATH } from '../../../core/router/Routes';
 import { clearProfile } from '../ProfileActions';
 
@@ -19,7 +19,7 @@ const PremiumProfileRouter = () => {
 
   return (
     <Switch>
-      <Route component={ProfileContainer} path={`${PROFILE_PATH}/helpline`} />
+      <Route component={HelplineContainer} path={`${PROFILE_VIEW_PATH}/helpline`} />
       <Redirect strict exact from={`${PROFILE_VIEW_PATH}/`} to={PROFILE_VIEW_PATH} />
       <Route
           component={PremiumProfileContainer}
