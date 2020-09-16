7d2f45b9e7661462f264638d23b8dec00dd5d9dc
Solomon Tang <solomoncztang@gmail.com>
2020-09-16 01:09:33 -0700

    linting

--- src/containers/profile/premium/styled/layout.js
+++ src/containers/profile/premium/styled/layout.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
 const AbsoluteCenter = styled.div`
-    align-items: center;
-    display: flex;
-    flex-direction: column;
-    flex: 1 0 auto;
-    justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  flex: 1 0 auto;
+  justify-content: center;
 `;
 
 export {
