f667c9570b7962f4c291e2c094fae5c775c18c7a
Solomon Tang <solomoncztang@gmail.com>
2020-09-18 18:15:05 -0700

    flex-start

--- src/containers/explore/FileResult.js
+++ src/containers/explore/FileResult.js
@@ -38,7 +38,7 @@ const { formatAsDate } = DateTimeUtils;
 const HeaderRow = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: center;
+  align-items: flex-start;
 `;
 
 const MinWidth = styled.div`
