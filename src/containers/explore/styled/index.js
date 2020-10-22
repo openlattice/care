b04632dec7c1376d9b37093888f9f92281924898
Solomon Tang <solomoncztang@gmail.com>
2020-10-21 23:57:17 -0700

    Add DetailWrapper and WordBreak style

--- src/containers/explore/styled/index.js
+++ src/containers/explore/styled/index.js
@@ -4,6 +4,22 @@ const ExploreResultsWrapper = styled.div`
   margin: 20px 0;
 `;
 
+const DetailWrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+
+  > span {
+    min-width: 100px;
+  }
+`;
+
+const WordBreak = styled.div`
+  word-break: break-all;
+  min-width: 0;
+`;
+
 export {
-  ExploreResultsWrapper
+  DetailWrapper,
+  ExploreResultsWrapper,
+  WordBreak,
 };
