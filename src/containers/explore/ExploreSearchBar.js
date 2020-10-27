8826253ab03504a5d359b8e966d6780f2e7142aa
Solomon Tang <solomoncztang@gmail.com>
2020-10-27 02:58:34 -0700

    remove unused imports

--- src/containers/explore/ExploreSearchBar.js
+++ src/containers/explore/ExploreSearchBar.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import styled from 'styled-components';
 import { faSearch } from '@fortawesome/pro-regular-svg-icons';
@@ -13,7 +13,6 @@ import {
 import { useDispatch } from 'react-redux';
 
 import {
-  clearExploreResults,
   exploreCitations,
   exploreContactInformation,
   exploreFile,
