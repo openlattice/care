e9810d3a2b61c385993a952b3eacd2185912a8b9
Solomon Tang <solomoncztang@gmail.com>
2020-11-17 21:16:27 -0800

    updateBasics calls createMissingPersonDetails

--- src/containers/profile/edit/basicinformation/sagas/BasicInformationSagas.js
+++ src/containers/profile/edit/basicinformation/sagas/BasicInformationSagas.js
@@ -7,7 +7,15 @@ import {
   takeEvery,
   takeLatest,
 } from '@redux-saga/core/effects';
-import { List, Map, fromJS } from 'immutable';
+import {
+  List,
+  Map,
+  fromJS,
+  getIn,
+  hasIn,
+  removeIn,
+} from 'immutable';
+import { DataProcessingUtils } from 'lattice-fabricate';
 import {
   DataApiActions,
   DataApiSagas,
@@ -15,6 +23,7 @@ import {
   SearchApiSagas,
 } from 'lattice-sagas';
 import { LangUtils, Logger, ValidationUtils } from 'lattice-utils';
+import { DateTime } from 'luxon';
 import type { SequenceAction } from 'redux-reqseq';
 
 import { getAddressWorker } from './AddressSagas';
@@ -23,25 +32,29 @@ import { getContactWorker } from './ContactSagas';
 import { getPhotosWorker } from './PhotosSagas';
 import { getScarsMarksTattoosWorker } from './ScarsMarksTattoosSagas';
 
-import { submitPartialReplace } from '../../../../../core/sagas/data/DataActions';
-import { submitPartialReplaceWorker } from '../../../../../core/sagas/data/DataSagas';
+import { submitDataGraph, submitPartialReplace } from '../../../../../core/sagas/data/DataActions';
+import { submitDataGraphWorker, submitPartialReplaceWorker } from '../../../../../core/sagas/data/DataSagas';
+import { COMPLETED_DT_FQN, VETERAN_STATUS_FQN } from '../../../../../edm/DataModelFqns';
 import { APP_TYPES_FQNS } from '../../../../../shared/Consts';
 import { getESIDsFromApp } from '../../../../../utils/AppUtils';
 import { groupNeighborsByFQNs } from '../../../../../utils/DataUtils';
 import { ERR_ACTION_VALUE_NOT_DEFINED, ERR_ACTION_VALUE_TYPE } from '../../../../../utils/Errors';
 import {
   constructFormDataFromNeighbors,
+  getEntityIndexToIdMapFromDataGraphResponse,
   getEntityIndexToIdMapFromNeighbors
 } from '../../../../reports/crisis/CrisisReportUtils';
 import { getAddress } from '../actions/AddressActions';
 import {
+  CREATE_MISSING_PERSON_DETAILS,
   GET_BASICS,
   GET_BASIC_INFORMATION,
   UPDATE_BASICS,
+  createMissingPersonDetails,
   getAppearance,
   getBasicInformation,
   getBasics,
-  updateBasics
+  updateBasics,
 } from '../actions/BasicInformationActions';
 import { getContact } from '../actions/ContactActions';
 import { getPhotos } from '../actions/PhotosActions';
@@ -58,6 +71,15 @@ const { getEntityData } = DataApiActions;
 const { getEntityDataWorker } = DataApiSagas;
 const { searchEntityNeighborsWithFilter } = SearchApiActions;
 const { searchEntityNeighborsWithFilterWorker } = SearchApiSagas;
+
+const {
+  findEntityAddressKeyFromMap,
+  getEntityAddressKey,
+  processAssociationEntityData,
+  processEntityData,
+  processEntityDataForPartialReplace,
+  replaceEntityAddressKeys,
+} = DataProcessingUtils;
 
 function* getBasicsWorker(action :SequenceAction) :Generator<any, any, any> {
   const response = {};
@@ -109,7 +131,7 @@ function* getBasicsWorker(action :SequenceAction) :Generator<any, any, any> {
     if (personResponse.error) throw personResponse.error;
     if (neighborsResponse.error) throw neighborsResponse.error;
 
-    const neighbors = fromJS(neighborsResponse.data).get(entityKeyId);
+    const neighbors = fromJS(neighborsResponse.data).get(entityKeyId) || List();
     const appTypeFqnsByIds = yield select((state) => state.getIn(['app', 'selectedOrgEntitySetIds']).flip());
     const personData = fromJS(personResponse.data);
     const personEntity = fromJS({ neighborDetails: personResponse.data });
@@ -142,13 +164,123 @@ function* getBasicsWatcher() :Generator<any, any, any> {
   yield takeLatest(GET_BASICS, getBasicsWorker);
 }
 
+function* createMissingPersonDetailsWorker(action :SequenceAction) :Generator<any, any, any> {
+  const response = {};
+  try {
+    const { value } = action;
+    if (!isDefined(value)) throw ERR_ACTION_VALUE_NOT_DEFINED;
+    yield put(createMissingPersonDetails.request(action.id));
+
+    const {
+      entityIndexToIdMap,
+      formData,
+      path,
+    } = value;
+    const section = path[0];
+    const personDetailsAddress = getEntityAddressKey(0, PERSON_DETAILS_FQN, VETERAN_STATUS_FQN);
+    const hasPersonDetailsData = hasIn(formData, [section, personDetailsAddress]);
+    const hasPersonDetailsEntityKey = hasIn(entityIndexToIdMap, [PERSON_DETAILS_FQN, 0]);
+    if (hasPersonDetailsData && !hasPersonDetailsEntityKey) {
+      const entitySetIds = yield select((state) => state.getIn(['app', 'selectedOrgEntitySetIds'], Map()));
+      const propertyTypeIds = yield select((state) => state.getIn(['edm', 'fqnToIdMap'], Map()));
+
+      const personDetailsFormData = fromJS({
+        [section]: {
+          [personDetailsAddress]: getIn(formData, [section, personDetailsAddress])
+        }
+      });
+
+      const entityData = processEntityData(personDetailsFormData, entitySetIds, propertyTypeIds);
+
+      const personEKID = getIn(entityIndexToIdMap, [PEOPLE_FQN, 0]);
+
+      const NOW_DATA = { [COMPLETED_DT_FQN]: [DateTime.local().toISO()] };
+      const associationEntityData = processAssociationEntityData(
+        [[REPORTED_FQN, personEKID, PEOPLE_FQN, 0, PERSON_DETAILS_FQN, NOW_DATA]],
+        entitySetIds,
+        propertyTypeIds
+      );
+
+      const dataGraphResponse = yield call(
+        submitDataGraphWorker,
+        submitDataGraph({
+          entityData,
+          associationEntityData,
+        })
+      );
+      if (dataGraphResponse.error) throw dataGraphResponse.error;
+
+      const appTypeFqnsByIds = yield select((state) => state.getIn(['app', 'selectedOrgEntitySetIds']).flip());
+
+      const newEntityIndexToIdMap = getEntityIndexToIdMapFromDataGraphResponse(
+        fromJS(dataGraphResponse.data),
+        schema,
+        appTypeFqnsByIds
+      );
+
+      response.data = {
+        formData: fromJS(removeIn(formData, [section, personDetailsAddress])),
+        entityIndexToIdMap: newEntityIndexToIdMap
+      };
+    }
+
+    yield put(createMissingPersonDetails.success(action.id, response.data));
+  }
+  catch (error) {
+    LOG.error(action.type, error);
+    response.error = error;
+    yield put(createMissingPersonDetails.failure(action.id));
+  }
+  finally {
+    yield put(createMissingPersonDetails.finally(action.id));
+  }
+  return response;
+}
+
+function* createMissingPersonDetailsWatcher() :Generator<any, any, any> {
+  yield takeEvery(CREATE_MISSING_PERSON_DETAILS, createMissingPersonDetailsWorker);
+}
+
 function* updateBasicsWorker(action :SequenceAction) :Generator<any, any, any> {
   try {
     const { value } = action;
     if (!isDefined(value)) throw ERR_ACTION_VALUE_NOT_DEFINED;
 
     yield put(updateBasics.request(action.id, value));
-    const response = yield call(submitPartialReplaceWorker, submitPartialReplace(value));
+
+    const createMissingPersonDetailsResponse = yield call(
+      createMissingPersonDetailsWorker,
+      createMissingPersonDetails(value)
+    );
+    if (createMissingPersonDetailsResponse.error) throw createMissingPersonDetailsResponse.error;
+
+    let { entityData } = value;
+    const adjustedFormData = createMissingPersonDetailsResponse?.data?.formData;
+    if (adjustedFormData) {
+      const draftWithKeys = replaceEntityAddressKeys(
+        adjustedFormData,
+        findEntityAddressKeyFromMap(value.entityIndexToIdMap)
+      );
+
+      const originalWithKeys = replaceEntityAddressKeys(
+        {},
+        findEntityAddressKeyFromMap(value.entityIndexToIdMap)
+      );
+
+      const entitySetIds = yield select((state) => state.getIn(['app', 'selectedOrgEntitySetIds'], Map()));
+      const propertyTypeIds = yield select((state) => state.getIn(['edm', 'fqnToIdMap'], Map()));
+      entityData = processEntityDataForPartialReplace(
+        draftWithKeys,
+        originalWithKeys,
+        entitySetIds,
+        propertyTypeIds,
+      );
+    }
+
+    const response = yield call(submitPartialReplaceWorker, submitPartialReplace({
+      ...value,
+      entityData,
+    }));
 
     if (response.error) throw response.error;
 
@@ -232,10 +364,12 @@ function* getBasicInformationWatcher() :Generator<any, any, any> {
 }
 
 export {
+  createMissingPersonDetailsWatcher,
+  createMissingPersonDetailsWorker,
   getBasicInformationWatcher,
   getBasicInformationWorker,
   getBasicsWatcher,
   getBasicsWorker,
   updateBasicsWatcher,
-  updateBasicsWorker
+  updateBasicsWorker,
 };
