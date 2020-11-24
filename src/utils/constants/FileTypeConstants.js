9b7d687e9c0b03161aaccb80f42c4b3cb725de7e
Solomon Tang <solomoncztang@gmail.com>
2020-11-24 04:07:51 -0800

    add more mime type

--- src/utils/constants/FileTypeConstants.js
+++ src/utils/constants/FileTypeConstants.js
@@ -7,3 +7,6 @@ export const ALL_VIDEO_MIME_TYPE = `${VIDEO_TYPE_PREFIX}*`;
 export const ALL_AUDIO_MIME_TYPE = `${AUDIO_TYPE_PREFIX}*`;
 export const PDF_MIME_TYPE = 'application/pdf';
 export const DOCX_MIME_TYPE = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
+export const TEXT_CSV = 'text/csv';
+export const TEXT_XML = 'text/xml';
+export const XML_HEADER = "<?xml version='1.0' encoding='UTF-8'?>";
