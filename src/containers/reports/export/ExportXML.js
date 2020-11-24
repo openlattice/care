e0d78951629f02daf531f7bff3feff15457ec2c9
Solomon Tang <solomoncztang@gmail.com>
2020-11-24 07:20:16 -0800

    lint

--- src/containers/reports/export/ExportXML.js
+++ src/containers/reports/export/ExportXML.js
@@ -2,13 +2,12 @@
 import React, { useEffect } from 'react';
 
 import { IconSplash, Spinner } from 'lattice-ui-kit';
-import { useRequestState } from 'lattice-utils';
 import { useDispatch } from 'react-redux';
 
-import { EXPORT_CRISIS_XML, exportCrisisXML } from './ExportActions';
+import { exportCrisisXML } from './ExportActions';
 
 const ExportXML = () => {
-  const requestState = useRequestState(['crisisReport', EXPORT_CRISIS_XML]);
+  // const requestState = useRequestState(['crisisReport', EXPORT_CRISIS_XML]);
 
   const dispatch = useDispatch();
   useEffect(() => {
