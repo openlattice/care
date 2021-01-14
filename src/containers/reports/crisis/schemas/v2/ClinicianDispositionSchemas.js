03054f48f0c8e5364c1a484e92d01e6261ccc4e5
Solomon Tang <solomoncztang@gmail.com>
2021-01-14 11:57:22 -0800

    disposition users OtherRadioWidget

--- src/containers/reports/crisis/schemas/v2/ClinicianDispositionSchemas.js
+++ src/containers/reports/crisis/schemas/v2/ClinicianDispositionSchemas.js
@@ -51,7 +51,7 @@ const schema = {
         // change this to disposition
         [getEntityAddressKey(0, DISPOSITION_CLINICIAN_FQN, FQN.CJ_DISPOSITION_FQN)]: {
           type: 'array',
-          description: SELECT_ALL_THAT_APPLY,
+          description: SELECT_ONLY_ONE,
           title: 'Disposition',
           items: {
             type: 'string',
@@ -143,12 +143,10 @@ const uiSchema = {
     },
     [getEntityAddressKey(0, DISPOSITION_CLINICIAN_FQN, FQN.CJ_DISPOSITION_FQN)]: {
       classNames: 'column-span-12',
-      'ui:widget': 'checkboxes',
+      'ui:widget': 'OtherRadioWidget',
       'ui:options': {
         mode: 'button',
         row: true,
-        withOther: true,
-        withNone: true,
       }
     },
     [getEntityAddressKey(0, REFERRAL_REQUEST_FQN, FQN.SERVICE_TYPE_FQN)]: {
