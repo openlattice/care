016a2a2954e51ebced8aa8e17c8d9d8256017c08
Solomon Tang <solomoncztang@gmail.com>
2021-06-04 13:52:56 -0700

    fix banner text color

--- src/containers/profile/RecentIncidentCard.js
+++ src/containers/profile/RecentIncidentCard.js
@@ -1,11 +1,16 @@
 // @flow
 import React from 'react';
 
+import styled from 'styled-components';
 import { faBullhorn } from '@fortawesome/pro-solid-svg-icons';
 import {
   Banner,
   Card,
 } from 'lattice-ui-kit';
+
+const StyledBanner = styled(Banner)`
+  color: inherit;
+`;
 
 type Props = {
   count :number;
@@ -20,13 +25,13 @@ const RecentIncidentCard = (props :Props) => {
 
   return (
     <Card>
-      <Banner
+      <StyledBanner
           isOpen
           icon={faBullhorn}>
         <span>
           {`${count} incident(s) within the last 7 days.`}
         </span>
-      </Banner>
+      </StyledBanner>
     </Card>
   );
 };
