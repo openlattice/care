1653c6ddac74da65486e7d2232b34e646df6386d
Solomon Tang <solomoncztang@gmail.com>
2021-09-22 14:12:21 -0700

    update initial state

--- src/core/redux/constants/index.js
+++ src/core/redux/constants/index.js
@@ -1,8 +1,12 @@
 import { ReduxConstants } from 'lattice-utils';
+import { RequestStates } from 'redux-reqseq';
 
 const { REQUEST_STATE } = ReduxConstants;
 
 const CRISIS_REPORT = 'crisisReport';
+export const RS_INITIAL_STATE = {
+  [REQUEST_STATE]: RequestStates.STANDBY,
+};
 
 export {
   CRISIS_REPORT,
