96732f3f2d866d67cdd94a56918135aaa31263af
Solomon Tang <solomoncztang@gmail.com>
2021-09-29 13:50:31 -0700

    selectors index.js

---
+++ src/core/redux/selectors/index.js
@@ -0,0 +1,3 @@
+export { default as selectApp } from './selectApp';
+export { default as selectPropertyTypeId } from './selectPropertyTypeId';
+export { default as selectEntitySetId } from './selectEntitySetId';
