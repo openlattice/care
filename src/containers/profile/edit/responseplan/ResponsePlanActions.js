5e19202b19e33e5c64f7c9976d84b12cbcd8a430
Solomon Tang <solomoncztang@gmail.com>
2022-01-12 01:14:18 -0800

    getAllResponsePlansExport actions

--- src/containers/profile/edit/responseplan/ResponsePlanActions.js
+++ src/containers/profile/edit/responseplan/ResponsePlanActions.js
@@ -13,12 +13,17 @@ const updateResponsePlan = newRequestSequence(UPDATE_RESPONSE_PLAN);
 const DELETE_INTERACTION_STRATEGIES :'DELETE_INTERACTION_STRATEGIES' = 'DELETE_INTERACTION_STRATEGIES';
 const deleteInteractionStrategies = newRequestSequence(DELETE_INTERACTION_STRATEGIES);
 
+const GET_ALL_RESPONSE_PLANS_EXPORT :'GET_ALL_RESPONSE_PLANS_EXPORT' = 'GET_ALL_RESPONSE_PLANS_EXPORT';
+const getAllResponsePlansExport = newRequestSequence(GET_ALL_RESPONSE_PLANS_EXPORT);
+
 export {
   DELETE_INTERACTION_STRATEGIES,
+  GET_ALL_RESPONSE_PLANS_EXPORT,
   GET_RESPONSE_PLAN,
   SUBMIT_RESPONSE_PLAN,
   UPDATE_RESPONSE_PLAN,
   deleteInteractionStrategies,
+  getAllResponsePlansExport,
   getResponsePlan,
   submitResponsePlan,
   updateResponsePlan,
